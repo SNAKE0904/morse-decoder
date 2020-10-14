9383b75da3e741698136ab902e59c320e44f0f98
Snake01 <vanya.kirdun@gmail.com>
2020-10-14 10:30:09 +0300

    init: compleate tasks

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let mass = [];
+    let morse = [];
+    let str = [];
+    let masss = []
+
+    for(let x = 0; x<expr.length/10; x++){
+        mass.push(expr.slice((x*10), (x*10)+10));
+    }
+
+    for(let x = 0; x<mass.length; x++){
+        for(let z = 0; z<mass[x].length/2;z++){
+            if(mass[x][z*2]=='1' && mass[x][(z*2)+1]=='0'){
+                str+='.'
+            } else if(mass[x][z*2]=='1' && mass[x][(z*2)+1]=='1'){
+                str+='-'
+            } else if(mass[x]=="**********"){
+                str+=' '
+                break;
+            }
+        }
+        morse.push(str);
+        str = ''
+    }
+
+    let key = Object.keys(MORSE_TABLE)
+    let values = Object.values(MORSE_TABLE)
+
+    for(let x = 0; x<morse.length; x++){
+        if(morse[x]==' '){
+            masss.push(' ')
+        }
+        for(let z = 0; z<key.length; z++){
+            if(morse[x]==key[z]){
+                masss.push(values[z])
+            }
+        }
+    }
+    return masss.join('');
 }
 
 module.exports = {
